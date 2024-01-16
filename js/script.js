b7c54892ef81d892b2b7790061ab93140608b690
jrazec <johnrazeagno@gmail.com>
2024-01-16 15:39:56 +0800

    Create js folder; Move script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* DATA TYPES IN INSERTION */ 
+
+
+
+
+const csvFormat = `Name, Age, Sex, Number, Birthday
+John,13,M,123,
+Jerome,14,M,124,1-2-2023
+Girl,12,F,125,1-3-2023
+Boy,,M,126,1-1-2023`;
+
+console.log(csvFormat)
