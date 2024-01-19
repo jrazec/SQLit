504b9e9dec3dff60ee938791412d731d42ca4d27
jrazec <johnrazeagno@gmail.com>
2024-01-19 21:01:21 +0800

    Add convToValuesStatement & convInsertion

--- js/utils/conversion/insertion.js
+++ js/utils/conversion/insertion.js
@@ -1,54 +1,40 @@
-/* import initializeInsertion from "/conversion/insertion" */
+/* import convInsertion from "/conversion/insertion" */
 
+export default function convInsertion(obj) {
+    const table = obj.table.toString();
+    const att = obj.att.toString();
+    const val = obj.val;
+    const withQuote = obj.withQuotation;
 
-/*            o Information o
-    - There are 3 common data types in sql which are
-        ::> strings, numbers and dates
-        > with quotations & without quatations
-          -char,varchars    -decimal,int,double
-          -texts,binary     -booleans
-          -dates            -bits and bytes
-        
-        - a -list of objects- can be used to store these info
-        which it will be the determiner if ever a certain column 
-        is inside squotation or without
+    const attribute = `INSERT INTO \n\t${table}( ${att} )\n`;
+    const value = convToValuesStatement(val,withQuote);
 
-
-*/
-
-/*              ---NEEDS:-----                                                     
-    - count of the attribute to be stored in 
-            ::> attributeCount [int]
-    - data types for each of the attributes to be stored in
-            ::> dataTypes [array] - thinking of obj here aswell if more feasible
-
-    - code the logic part which is to convert the strings to insertion
-    - initialize a list of objects as determiner
-    - there, proceed to the checking--if else, base on the user input, is it
-    part of the quotation or is it not quoted - using of for loop
-    - trying the filelist on the sheet.csv if it the logic would work 
-*/
-// MySQL only
-const determiner = {
-    withQuotes: ["CHAR","VARCHAR","BINARY","VARBINARY","TEXT","MEDIUMTEXT","LONGTEXT","DATE","DATETIME","TIMESTAMP","TIME","YEAR"],
-    wOutQuotes: ["INT","BIGINT","DOUBLE","DECIMAL","BOOL","BIT"]
-}; 
-
-`
-INSERT INTO 
-    table_1($ {attributes})
-VALUES
-    ({Values}),
-
-    ...
-    ({Values});
-`
-
-function initializeInsertion(values,prompt) {
-    
+    const insertStatement = attribute + value; // Added this just for formality/readability
+    return insertStatement;
 }
 
 
 
+
+function convToValuesStatement(val,withQuote){
+    let value = "VALUES \n\t";
+
+    for(let row = 0; row < val.length; row++){ 
+        value += "( ";
+
+        for (let col = 0; col < val[row].length;col++){
+            if(withQuote[col]){
+                value += (val[row][col] !== "") ? `'${val[row][col]}'` : "null";  // If the csv file dont have a value, it will be null
+            }else{
+                value += (val[row][col] !== "") ? val[row][col] : "null";
+            }
+
+            (col !== val[row].length-1) ? value += ", " : value += " ";  // the last col will have whitespace instead of ,     
+        }
+
+        (row === val.length-1) ? value += ");" : value += "),\n\t"; // the last row will have ); instead of ),
+    }
+    return value;
+}
+
 // This would be removed once it is already in selection_dd
-initializeInsertion();
